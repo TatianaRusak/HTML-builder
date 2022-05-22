3b01108538a757ef26d1be54856fd0e75c8a652c
Tatiana Rusak <tatianarusak85@gmail.com>
2022-05-22 21:59:41 +0300

    fix: get rid of sync

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,23 +1,30 @@
 const path = require('path');
 const fs = require('fs');
+const { readdir } = require ('fs/promises');
 
 const pathToDir = path.join(__dirname, 'styles');
 const pathToBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
-const files = fs.readdirSync(pathToDir, { withFileTypes: true })
-  .filter(item => item.isFile())
-  .filter(item => path.extname(item.name) === '.css')
-  .map(item => item.name);
-
-files.forEach((file) => { 
-  const readStream = fs.createReadStream(path.join(pathToDir, file), 'utf-8');
-  readStream.on('data', (chunk) => {
-    fs.appendFile(
-      pathToBundle,
-      chunk,
-      err => {
-        if (err) { throw err; }
-      }
-    );
-  });
-});
+(async () =>  {
+  try {
+    const filesInDir = await readdir(pathToDir, { withFileTypes: true });
+    let files = filesInDir.filter(item => item.isFile())
+      .filter(item => path.extname(item.name) === '.css')
+      .map(item => item.name);
+    
+    files.forEach((file) => { 
+      const readStream = fs.createReadStream(path.join(pathToDir, file), 'utf-8');
+      readStream.on('data', (chunk) => {
+        fs.appendFile(
+          pathToBundle,
+          chunk,
+          err => {
+            if (err) { throw err; }
+          }
+        );
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})();
