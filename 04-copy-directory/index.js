3298e2658b252df3b39fb3d4274a6f6019049d7f
Tatiana Rusak <tatianarusak85@gmail.com>
2022-05-24 22:22:03 +0300

    fix: fix bug with folder updating

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,28 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.access('04-copy-directory/files-copy', function(err) {
-  if (err && err.code === 'ENOENT') {
-    fs.mkdir('04-copy-directory/files-copy', {recursive: true}, (err) => { // создать папку, если такой еще нет
-      if (err) { 
-        throw err;
-      }
-    });
-    console.log('Папка успешно создана');
+fs.mkdir('04-copy-directory/files-copy', { recursive: true }, (err) => { // создать папку, если такой еще нет
+  if (err) {
+    throw err;
   }
 });
 
-fs.readdir('04-copy-directory/files', (err, files) => {
-  if (err) throw err;
-  files.forEach((file) => { 
-    let pathFrom = path.join(__dirname, 'files', file);
-    let pathTo = path.join(__dirname, 'files-copy', file);
-  
-    fs.copyFile(pathFrom, pathTo, (err) => { 
-      if (err) { 
-        throw err;
-      }
+async function emptyFolder() { 
+  fs.readdir('04-copy-directory/files-copy', (err, files) => {
+    if (err) throw err; // не прочитать содержимое папки
+    
+    files.forEach(file => { 
+      fs.unlink(`04-copy-directory/files-copy/${file}`, err => {
+        if(err) throw err; // не удалось удалить файл
+      });
     });
   });
-  console.log('Ура, все файлы скопированы!');
-});
+}
+
+async function copyFiles() { 
+  fs.readdir('04-copy-directory/files', (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => { 
+      let pathFrom = path.join(__dirname, 'files', file);
+      let pathTo = path.join(__dirname, 'files-copy', file);
+    
+      fs.copyFile(pathFrom, pathTo, (err) => { 
+        if (err) { 
+          throw err;
+        }
+      });
+    });
+    console.log('Ура, все файлы скопированы!');
+  });
+}
+
+
+(async () => { 
+  // await createFolder();
+  await emptyFolder();
+  await copyFiles();
+})();
